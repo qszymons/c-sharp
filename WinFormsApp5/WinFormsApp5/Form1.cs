6991ab3fed7a16bc2f32d74dc0e42d04c6713943
Szymon Olejnik <77973139+qszymons@users.noreply.github.com>
2024-11-20 09:22:18 +0100

    zad5

---
+++ WinFormsApp5/WinFormsApp5/Form1.cs
@@ -0,0 +1,23 @@
+namespace WinFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e){
+            public class NodeG
+        {
+            List<NodeG>s¹siedzi = new List<NodeG>();
+            int data;
+            public override string ToString()
+            {
+                return this.data.ToString();
+            }
+        }
+    
+        }
+    }
+}
---
+++ WinFormsApp5/WinFormsApp5/Grafs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WinFormsApp5.Form1;
+
+namespace WinFormsApp5
+{
+    internal class Grafs
+    {
+        List<NodeG> nodes = new List<NodeG>();
+
+        public void dodajSomsiada(NodeG somsiad)
+        {
+            if (!this.nodes.Contains(somsiad))
+            {
+                this.nodes.Add(somsiad);
+            }
+        }
+        public void przechodzenie(NodeG somsiad)
+        {
+            for (int i = 0; i < this.nodes.Count; i++)
+            {
+                NodeG node = this.nodes[i];
+
+            }
+        }
+
+    }
+}
+
